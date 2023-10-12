019624c09038dad821da5d9134943c672221f3d5
Fajrin198 <fajrinpanji6969@gmail.com>
2023-10-12 21:44:49 +0800

    day4

---
+++ src/day4/main.java
@@ -0,0 +1,13 @@
+package day4;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Informatika");
+        }
+    }
+}
