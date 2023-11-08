cf0837a5690d48e2fc24859cd302670d8b9918d8
Fajrin198 <fajrinpanji6969@gmail.com>
2023-11-08 21:55:47 +0800

    day31

--- src/day31.java
+++ src/day31.java
@@ -5,9 +5,12 @@ public class day31 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Jumlah karyawan : ");
+        // menginputkan banyaknya karyawan
         int jumlahKaryawan = sc.nextInt();
         System.out.println("Jumlah hari kerja");
+        // nantinya digunakan untuk menampung jumlah hari kerja tiap karyawan
         int[] jumlahHariKerja = new int[jumlahKaryawan];
+        // untuk menginputkan jumlah hari kerja tiap karyawan sebanyak jumlah karyawan yang diinputkan tadi dan akan langsung memasukkannya ke array
         for (int i = 0; i < jumlahKaryawan; i++) {
             System.out.print("Karyawan "+(i+1)+" = ");
             jumlahHariKerja[i] = sc.nextInt();
@@ -18,9 +21,11 @@ public class day31 {
         int gajiBersihPerhari = gajiKotorPerhari-UangMakanPerhari;
         int totalUangUntukMenggajiSemuaKaryawan = 0;
         System.out.println("<==== GAJI BERSIH KARYAWAN ====>");
+        // akan menampilkan gaji bersih tiap karyawan
         for (int i = 0; i < jumlahKaryawan; i++) {
             int gajiBersihKaryawan = jumlahHariKerja[i]*gajiBersihPerhari;
             System.out.println("Karyawan "+(i+1)+" = "+gajiBersihKaryawan);
+            // setiap gaji bersih karyawan akan ditambahkan di variabel totalUangUntukMenggajiSemuaKaryawan
             totalUangUntukMenggajiSemuaKaryawan += gajiBersihKaryawan;
         }
         System.out.println("--------------------------------");
