0e1f76ee2a9c726ee4f50b6f21ae9510e3bde758
Fajrin198 <fajrinpanji6969@gmail.com>
2023-12-01 00:00:59 +0800

    day52

---
+++ src/Day53.java
@@ -0,0 +1,18 @@
+
+
+
+public class Day53 {
+    public static void main(String[] args) {
+        // Latihan membaca error
+        try {
+            int a = 5;
+            System.out.cetak(a);
+        } catch (Exception e) {
+            System.err.println("DITEMUKAN ERROR PROGRAM");
+            System.out.println(e.getLocalizedMessage());
+            Throwable fillInStackTrace = e.fillInStackTrace();
+            System.out.println(fillInStackTrace);
+            System.out.println(e.getMessage());
+        }
+    }
+}
