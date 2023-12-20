6c2ad8f9e002b1f75577156783a01eebe1502d8a
Fajrin198 <fajrinpanji6969@gmail.com>
2023-12-21 01:17:15 +0800

    day73

--- src/KonversiSatuan.java
+++ src/KonversiSatuan.java
@@ -12,42 +12,54 @@ import java.util.Scanner;
 public class KonversiSatuan {
 
     public static void main(String[] args) {
-        double nilai = 10;
-        Scanner sc = new Scanner(System.in);
-        String inputSatuan01 = sc.nextLine();
-        String inputSatuan02 = sc.nextLine();
-        String[] satuan = {"km", "hm", "dam", "m", "dm", "cm", "mm"};
-        int nilaiAwal = 0;
-        int nilaiAkhir = 0;
-        for (int i = 0; i < satuan.length; i++) {
-            if (!(inputSatuan01.equalsIgnoreCase(satuan[i]))) {
-                nilaiAwal++;
-            } else {
-                break;
-            }
-        }
-        for (int i = 0; i < satuan.length; i++) {
-            if (!(inputSatuan02.equalsIgnoreCase(satuan[i]))) {
-                nilaiAkhir++;
-            } else {
-                break;
-            }
-        }
-        System.out.println(nilaiAwal);
-        System.out.println(nilaiAkhir);
-        if (nilaiAwal < nilaiAkhir) {
-            for (int j = nilaiAwal; j < nilaiAkhir; j++) {
-                nilai *= 10;
-            }
-        } else {
-            System.out.println(nilai);
-            System.out.println(nilaiAwal);
-            System.out.println(nilaiAkhir);
-            for (int j = nilaiAwal; j > nilaiAkhir; j--) {
-                nilai /= 10.0;
-            }
-        }
-        System.out.println(1/1000000.0);
-        System.out.println(nilai);
+//        double nilai = 10;
+//        Scanner sc = new Scanner(System.in);
+//        String inputSatuan01 = sc.nextLine();
+//        String inputSatuan02 = sc.nextLine();
+//        String[] satuan = {"km", "hm", "dam", "m", "dm", "cm", "mm"};
+        double a = Math.pow(10, 3);
+        System.out.println(a);
+        double hasil = convert(1,3,3);
+        System.out.println(hasil);
+//        int nilaiAwal = 0;
+//        int nilaiAkhir = 0;
+//        for (int i = 0; i < satuan.length; i++) {
+//            if (!(inputSatuan01.equalsIgnoreCase(satuan[i]))) {
+//                nilaiAwal++;
+//            } else {
+//                break;
+//            }
+//        }
+//        for (int i = 0; i < satuan.length; i++) {
+//            if (!(inputSatuan02.equalsIgnoreCase(satuan[i]))) {
+//                nilaiAkhir++;
+//            } else {
+//                break;
+//            }
+//        }
+//        System.out.println(nilaiAwal);
+//        System.out.println(nilaiAkhir);
+//        if (nilaiAwal < nilaiAkhir) {
+//            for (int j = nilaiAwal; j < nilaiAkhir; j++) {
+//                nilai *= 10;
+//            }
+//        } else {
+//            System.out.println(nilai);
+//            System.out.println(nilaiAwal);
+//            System.out.println(nilaiAkhir);
+//            for (int j = nilaiAwal; j > nilaiAkhir; j--) {
+//                nilai /= 10.0;
+//            }
+//        }
+//        System.out.println(1/1000000.0);
+//        System.out.println(nilai);
+    }
+    
+    private static double convert(int dari, int ke,double value){
+        double result = 0;
+        double km = value/Math.pow(10, dari);
+        result = km*Math.pow(10, ke);
+        
+        return result;
     }
 }
