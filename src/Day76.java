8b7db2dfea6705f01370255c156886f9a4b8f83f
Fajrin198 <fajrinpanji6969@gmail.com>
2023-12-24 00:17:39 +0800

    day76

---
+++ src/Day76.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MY ASUS
+ */
+public class Day76 {
+    public static void main(String[] args) {
+        String[] arrStr = {"apple","orange","banana","watermelon","tomato"};
+        for (String arrStr1 : arrStr) {
+            System.out.println(arrStr1);
+        }
+        for (int i = 0; i < arrStr.length; i++) {
+            System.out.println(arrStr[i]);
+        }
+    }
+}
