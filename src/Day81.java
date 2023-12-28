6b6576c5a0fcf7f1a94f8af5d54435ae0c6b695c
Fajrin198 <fajrinpanji6969@gmail.com>
2023-12-29 00:57:21 +0800

    day81

---
+++ src/Day81.java
@@ -0,0 +1,28 @@
+
+
+import java.util.Scanner;
+
+public class Day81 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] dataArr = new int[m][n];
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                System.out.print("element{"+i+","+j+"] = ");
+                int element = sc.nextInt();
+                dataArr[i][j] = element;
+            }
+        }
+        
+        for(int i=0; i<dataArr.length; i++){
+            System.out.print("|");
+            for(int j=0; j<dataArr[i].length; j++){
+                System.out.print(dataArr[i][j]+"|");
+            }
+            System.out.println();
+        }   
+    }
+}
